9454418eab8c1d928ce837dcfe38a50f1f6753e8
kx-wang <60047597+kx-wang@users.noreply.github.com>
2020-05-26 11:31:56 -0400

    Update and rename Dijkstra_method.py to Astar_method.py
    -took out the condition to stop searching if current node = goal
    -changed to check the alternative distance for all neighbors, previously it was checking the alternative distance only when the neighbor cell is one that has not been examined

--- Astar_method.py
+++ Astar_method.py
@@ -3,6 +3,11 @@
 Created on Wed May 20 19:14:44 2020
 
 @author: Cathy
+
+Implements the A* algorithm to solve for shortest path from start to finish in a maze. 
+If the heuristic function h is set to zero, then Dijkstra's method is recovered
+
+Ref: https://en.wikipedia.org/wiki/A*_search_algorithm
 """
 
 import numpy as np
@@ -29,25 +34,32 @@ def get_neighbors(node):
                 
     return np.array(neighbors)
 
-def get_distance(node, neighbors, distance):
-    row, col = node
-    prev_dist = distance[row,col] 
+
+def calc_h(curNode, goal):
+    # using the Manhatan Distance Heuristic, ignores any obstacles 
+    x_start, y_start = curNode
+    x_goal, y_goal = goal 
+    h = abs(x_start-x_goal) + abs(y_start-y_goal)
+    return h 
+
+def reorder(toExamine, fScore, counter): 
+    ex = toExamine[0:counter]
     
-    min_dist = 1e3
-    for i in range(len(neighbors)):
-        row,col = neighbors[i]
-        distance[row,col] = prev_dist + 1
-        if distance[row,col] < min_dist:
-            min_dist = distance[row,col]
-            ind_min_dist = i
-        
-    return distance, ind_min_dist 
-
-
-
-#-----------------------------------------------------------------------------    
-
-maze = np.array([
+    f = np.zeros(shape = (counter))
+    for i in range(counter):
+        f[i] = fScore[int(ex[i,0]),int(ex[i,1])]
+    
+    f_ind = np.argsort(f)
+    
+    for i in range(counter):
+        toExamine[i,:] = [ex[f_ind[i],0],ex[f_ind[i],1]]
+    
+    return toExamine 
+#-----------------------------------------------------------------------------   
+#  some different mazes, starting points, ending points 
+n_problem = int(input("choose a maze (input 1, 2,or 3): "))
+if n_problem == 1:
+    maze = np.array([
     [ 0,  1,  0,  0,  0,  0,  0,  0,  0,  0],
     [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
     [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
@@ -58,37 +70,58 @@ maze = np.array([
     [ 0,  0,  0,  0,  0,  0,  1,  1,  1,  1],
     [ 0,  1,  1,  1,  1,  1,  0,  0,  0,  0],
     [ 0,  0,  0,  0,  0,  0,  0,  1,  0,  0]
-])
-'''
-maze = np.array([
+    ])
+    goal = np.array([9,9])
+    start = np.array([0,0])
+elif n_problem == 2: 
+    maze = np.array([
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  1],
     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  1,  0,  0],
     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+    [ 0,  0,  1,  0,  0,  0,  0,  0,  1,  0],
     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
-])
-goal = np.array([5,9])
-start = np.array([5,0])
-'''
+    ])
+    goal = np.array([4,9])
+    start = np.array([5,0])
+else: 
+    maze = np.array([
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  1,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    ])
+    goal = np.array([5,9])
+    start = np.array([5,0])
+
+
+#------------------------------------------------------------------------------------------------
+
+# number of rows and columns of the maze 
 n_row = 10
 n_col = 10 
-goal = np.array([9,9])
-start = np.array([0,0])
-
 
 # will keep lists of cells that have/have not been examined yet
-toExamine = np.zeros( shape = (1,2) ) # priority list 
+toExamine = np.zeros( shape = (1,2) ) # this will be priority list, based on distance to starting point or f score  
 alreadyExamined = np.zeros( shape = (1,2)  )
 
 curNode = start # current node    
+toExamine[0] = start
 
 distance = np.inf*np.ones(shape=(n_row,n_col)) # matrix to keep track of all the distances from a node to the starting cell 
-distance[0,0] = 0
+distance[start[0],start[1]] = 0
+fScore = np.inf*np.ones( shape = (n_row,n_col) )
+fScore[start[0],start[1]] = calc_h(start,goal)
 
 path = []
 previous = np.zeros(shape = (n_row,n_col))
@@ -96,8 +129,6 @@ previous = np.zeros(shape = (n_row,n_col))
 # while there are still nodes to examine      
 while len(toExamine) > 0: 
     curNode = toExamine[0] # set the current node to the one that is at the top of the priority queue
-    #path.append(curNode)
-    #print(curNode)
     
     # get the valid neighbors of the current node 
     neighbors = get_neighbors(curNode) 
@@ -111,32 +142,30 @@ while len(toExamine) > 0:
     row, col = curNode 
     dist_curNode = distance[int(row),int(col)]
     
+    counter = 0 # this will be used to re-order the nodes in the toExamine list according to their fScore
     for i in range(len(neighbors)):  
-        
-        # check if the neighbor has already been examined (ie has been in priority queue and then deleted)
-        # if not, then proceed with the following 
-        if np.size(np.where( (alreadyExamined[:,0] == neighbors[i,0]) & (alreadyExamined[:,1]==neighbors[i,1]))) == 0:
-          
-            row, col = neighbors[i]
-            alt_dist = dist_curNode + 1
-        
-            # if there is an alternative distance of the neighbor node that is smaller than the known distance    
-            # then update the record with the smaller distance        
-            if alt_dist < distance[int(row),int(col)]: 
-                distance[int(row), int(col)] = alt_dist
-                previous[int(row),int(col)] = cell2ind(curNode)
-                
-            # since for all the nodes, the lengths to its neighbors are the same, then for the next node 
-            # to examine, the priorities will be the same 
-            toExamine = np.insert(toExamine, 0, neighbors[i], axis=0)
+              
+        row, col = neighbors[i]
+        alt_dist = dist_curNode + 1
+        h = calc_h(neighbors[i], goal)
             
-    #if len(neighbors) == 0: 
-    #    path.remove(path[-1])
+        # if there is an alternative distance of the neighbor node that is smaller than the known distance    
+        # then update the record with the smaller distance        
+        if alt_dist < distance[int(row),int(col)]: 
+            distance[int(row), int(col)] = alt_dist
+            fScore[int(row), int(col)] = alt_dist + h
+            previous[int(row),int(col)] = cell2ind(curNode)
         
-    if (curNode[0] == goal[0]) and (curNode[1] == goal[1]): 
-        break 
+            # if the neighbor node is not in the list of nodes to examine, and it has a shorter distance
+            # then it should be examined 
+            if np.size(np.where( (toExamine[:,0] == neighbors[i,0]) & (toExamine[:,1]==neighbors[i,1]))) == 0:
+                toExamine = np.insert(toExamine, 0, neighbors[i], axis=0)
+                counter = counter + 1        
 
+    if counter !=0:    
+        toExamine = reorder(toExamine, fScore, counter) # prioritize/reorder the first counter number of nodes to examine, based on their f score
 
+            
 # --------------------------construct the path--------------------------------- 
 ind2cell = np.zeros(shape = (n_row*n_col,2))
 counter = 0
